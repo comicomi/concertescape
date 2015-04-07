7183ce963e827102f27debeff501902bdad7b75b
comi <comi.mic@outlook.com>
2015-04-07 21:50:34 +0200

    First commit - Basic routing implemented. Index.html page styled with style.css added.

---
+++ src/concertescape/app.clj
@@ -0,0 +1,22 @@
+(ns concertescape.app
+  (use (compojure handler [core :only (GET POST defroutes)]))
+  (require [ring.adapter.jetty :as jetty]
+   [compojure.route]
+   [net.cgrand.enlive-html :as en]
+   [ring.util.response :as response]) 
+)
+
+
+(en/deftemplate homepage
+  (en/xml-resource "index.html") [request] 
+ )
+
+(defroutes app*
+  (compojure.route/resources "/")
+  (GET "/" request (homepage request))
+ )
+
+(def app (compojure.handler/site app*))
+
+(defn -main []
+  (jetty/run-jetty #'app {:port 8080 :join? false}))
